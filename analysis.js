75955aaa4bf6dec624c0cbc6df6868a6f0f96b28
LMR <dlaudfuf33@gmail.com>
2023-11-22 15:48:10 +0900

    웹페이지 ++ 시각화.js

--- analysis.js
+++ analysis.js
@@ -1,24 +1,42 @@
 // Vega-Lite을 사용한 시각화를 위한 함수
 const visualizeData = async () => {
-  // 'correlation_matrix.json' 파일을 비동기적으로 불러옴
-  const response = await fetch('파일명.json');
-  // 응답으로부터 JSON 데이터를 추출
-  const data = await response.json();
+  // 첫 번째 데이터셋 불러오기
+  const response1 = await fetch('파일명1.json');
+  const data1 = await response1.json();
 
-  // Vega-Lite을 위한 시각화 명세(specification) 정의
-  const spec = {
-      $schema: 'https://vega.github.io/schema/vega-lite/v4.json', // Vega-Lite 스키마 버전
-      description: 'A simple bar chart with embedded data.', // 시각화에 대한 간단한 설명
-      data: { values: data }, // 사용할 데이터셋 지정
-      mark: 'bar', // 막대 차트(mark) 사용
-      encoding: { // 데이터를 시각적 요소로 매핑하는 방법 정의
-          x: { field: 'category', type: 'ordinal' }, // x축에 나타낼 필드와 데이터 유형(범주형) 지정
-          y: { field: 'amount', type: 'quantitative' } // y축에 나타낼 필드와 데이터 유형(수치형) 지정
+  // 첫 번째 데이터셋에 대한 시각화 명세 (막대 차트)
+  const spec1 = {
+      $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
+      description: '첫 번째 그래프: 막대 차트',
+      data: { values: data1 },
+      mark: 'bar',
+      encoding: {
+          x: { field: 'category', type: 'ordinal' },
+          y: { field: 'amount', type: 'quantitative' }
       }
   };
 
-  // Vega-Embed를 사용하여 '#visualization' 요소에 시각화 embed
-  vegaEmbed('#visualization', spec);
+  // 첫 번째 데이터셋 시각화
+  vegaEmbed('#visualization1', spec1);
+
+  // 두 번째 데이터셋 불러오기
+  const response2 = await fetch('파일명2.json');
+  const data2 = await response2.json();
+
+  // 두 번째 데이터셋에 대한 시각화 명세 (원형 차트)
+  const spec2 = {
+      $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
+      description: '두 번째 그래프: 원형 차트',
+      data: { values: data2 },
+      mark: 'arc', // 원형 차트
+      encoding: {
+          theta: { field: 'amount', type: 'quantitative' }, // 원형 차트의 각도
+          color: { field: 'category', type: 'nominal' } // 색상으로 범주 구분
+      }
+  };
+
+  // 두 번째 데이터셋 시각화
+  vegaEmbed('#visualization2', spec2);
 };
 
 // 함수 실행으로 시각화 과정 시작
