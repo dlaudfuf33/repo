c4e811aaab27747c5fb4c4c3ebf3cfd6c3eeeb56
LMR <dlaudfuf33@gmail.com>
2023-11-22 16:29:06 +0900

    end

--- analysis.js
+++ analysis.js
@@ -1,43 +1,39 @@
-// Vega-Lite을 사용한 시각화를 위한 함수
 const visualizeData = async () => {
   // 첫 번째 데이터셋 불러오기
-  const response1 = await fetch('파일명1.json');
+  const response1 = await fetch('/gender_recid.json');
   const data1 = await response1.json();
 
   // 첫 번째 데이터셋에 대한 시각화 명세 (막대 차트)
   const spec1 = {
       $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
-      description: '첫 번째 그래프: 막대 차트',
+      description: '성별에 따른 일반적 재범 발생 여부',
       data: { values: data1 },
       mark: 'bar',
       encoding: {
-          x: { field: 'category', type: 'ordinal' },
-          y: { field: 'amount', type: 'quantitative' }
+          x: { field: 'sex', type: 'nominal', axis: { title: '성별' }},
+          y: { field: 'count', type: 'quantitative', axis: { title: '재범 수' }},
+          color: { field: 'is_recid', type: 'nominal', axis: { title: '재범 발생 여부' }}
       }
   };
-
-  // 첫 번째 데이터셋 시각화
   vegaEmbed('#visualization1', spec1);
 
   // 두 번째 데이터셋 불러오기
-  const response2 = await fetch('파일명2.json');
+  const response2 = await fetch('/race_violent_recid.json');
   const data2 = await response2.json();
 
-  // 두 번째 데이터셋에 대한 시각화 명세 (원형 차트)
+  // 두 번째 데이터셋에 대한 시각화 명세 (히트맵)
   const spec2 = {
       $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
-      description: '두 번째 그래프: 원형 차트',
+      description: '인종에 따른 폭력적 재범 발생 여부',
       data: { values: data2 },
-      mark: 'arc', // 원형 차트
+      mark: 'rect',
       encoding: {
-          theta: { field: 'amount', type: 'quantitative' }, // 원형 차트의 각도
-          color: { field: 'category', type: 'nominal' } // 색상으로 범주 구분
+          x: { field: 'race', type: 'nominal', axis: { title: '인종' }},
+          y: { field: 'is_violent_recid', type: 'nominal', axis: { title: '폭력적 재범 발생 여부' }},
+          color: { field: 'count', type: 'quantitative', legend: { title: '재범 수' }}
       }
   };
-
-  // 두 번째 데이터셋 시각화
   vegaEmbed('#visualization2', spec2);
 };
 
-// 함수 실행으로 시각화 과정 시작
 visualizeData();
--- data.py
+++ data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('C:/Users/218/Desktop/cox-violent-parsed_filt_usable.csv')
+data = pd.read_csv('/Users/lmr/Desktop/SE1115/1122/repo/compass.csv')
 
 selected_columns = ['sex', 'age', 'race', 'is_violent_recid', 'is_recid', 'event']
 df = data[selected_columns]
@@ -17,3 +17,13 @@ race_violent_recid_matrix = pd.crosstab(df['race'], df['is_violent_recid'])
 sns.heatmap(race_violent_recid_matrix, annot=True, fmt='d', cmap='Blues')
 plt.title('Race vs. Violent Recidivism')
 plt.show()
+
+# 성별과 일반적 재범 발생 여부 데이터 집계
+gender_recid = df.groupby(['sex', 'is_recid']).size().reset_index(name='count')
+
+# 인종과 폭력적 재범 발생 여부 데이터 집계
+race_violent_recid = df.groupby(['race', 'is_violent_recid']).size().reset_index(name='count')
+
+# JSON 파일로 저장
+gender_recid.to_json('/Users/lmr/Desktop/SE1115/1122/repo/gender_recid.json', orient='records')
+race_violent_recid.to_json('/Users/lmr/Desktop/SE1115/1122/repo/race_violent_recid.json', orient='records')
